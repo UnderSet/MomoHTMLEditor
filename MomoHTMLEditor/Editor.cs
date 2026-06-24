1e337cf3298e99eb124eb94d1f2438c6a6c1e6a1
UnderSet <80137504+UnderSet@users.noreply.github.com>
2026-06-25 01:41:02 +0700

    feat: next lines previews and message types

--- MomoHTMLEditor/Editor.cs
+++ MomoHTMLEditor/Editor.cs
@@ -15,7 +15,7 @@ namespace MomoHTMLEditor
         private string messageBuffer = "";
         private bool activeSenderBuffer = false;
         private int MessagesIndex = 0;
-        private MessageType Type = MessageType.Received;
+        private MessageType MsgType = MessageType.Received;
         private string activeBuffer
         {
             get => activeSenderBuffer ? senderBuffer : messageBuffer;
@@ -29,29 +29,93 @@ namespace MomoHTMLEditor
         {
             while (true)
             {
+                int width = Console.WindowWidth;
+                int freeLines = Console.WindowHeight - 3;
+                int peekLines = 0;
                 Console.Clear();
-                //for (int i = 0; i < MessagesBuffer.Count; i++) {
-                //    Console.WriteLine($"{i} - {MessagesBuffer[i].Text}");
-                //}
                 Console.ForegroundColor = ConsoleColor.DarkGray;
                 Console.WriteLine("[ESC] Menu [TAB] Sender/Message [ALT]+[UP/DOWN] Move Message");
                 Console.WriteLine("Type normally to enter text");
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine($"Index {MessagesIndex} | {fileName}");
+                Console.WriteLine($"Index {MessagesIndex + 1} | {fileName}");
+
+                // does a switch work here?
+                if (MsgType == MessageType.Received) {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    string dispSendBuf = (MessagesIndex + 1) + " >R |" + senderBuffer + (activeSenderBuffer ? "_" : "");
+                    string dispMesgBuf = (MessagesIndex + 1) + " >  |" + messageBuffer + (activeSenderBuffer ? "" : "_");
+                    freeLines = freeLines - (int)Math.Ceiling((decimal)dispSendBuf.Length / (decimal)width);
+                    freeLines = freeLines - (int)Math.Ceiling((decimal)dispMesgBuf.Length / (decimal)width);
+                    Console.WriteLine(dispSendBuf);
+                    Console.WriteLine(dispMesgBuf);
+                }
+                else if (MsgType == MessageType.Sent ) {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    string dispMesgBuf = (MessagesIndex + 1) + "  S<|" + messageBuffer + (activeSenderBuffer ? "" : "_");
+                    freeLines = freeLines - (int)Math.Ceiling((decimal)dispMesgBuf.Length / (decimal)width);
+                    Console.WriteLine(dispMesgBuf);
+                }
+                else if (MsgType == MessageType.System) {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    string dispMesgBuf = (MessagesIndex + 1) + " -N-|" + messageBuffer + (activeSenderBuffer ? "" : "_");
+                    freeLines = freeLines - (int)Math.Ceiling((decimal)dispMesgBuf.Length / (decimal)width);
+                    Console.WriteLine(dispMesgBuf);
+                }
+
+                while (freeLines > 1) {
+                    peekLines = peekLines + 1;
+                    if (MessagesIndex + peekLines < MessagesBuffer.Count) {
+                        var nextmsg = MessagesBuffer[MessagesIndex + peekLines];
+                        if (nextmsg.Type == MessageType.Received) {
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            string dispSendBuf = ">R |" + nextmsg.Sender;
+                            string dispMesgBuf = ">  |" + nextmsg.Text;
+                            freeLines = freeLines - (int)Math.Ceiling((decimal)dispSendBuf.Length / (decimal)width);
+                            freeLines = freeLines - (int)Math.Ceiling((decimal)dispMesgBuf.Length / (decimal)width);
+                            Console.WriteLine(dispSendBuf);
+                            Console.WriteLine(dispMesgBuf);
+                        }
+                        else if (nextmsg.Type == MessageType.Sent) {
+                            Console.ForegroundColor = ConsoleColor.DarkCyan;
+                            string dispMesgBuf = " S<|" + nextmsg.Text;
+                            freeLines = freeLines - (int)Math.Ceiling((decimal)dispMesgBuf.Length / (decimal)width);
+                            Console.WriteLine(dispMesgBuf);
+                        }
+                        else if (nextmsg.Type == MessageType.System) {
+                            Console.ForegroundColor = ConsoleColor.DarkGray;
+                            string dispMesgBuf = "-N-|" + nextmsg.Text;
+                            freeLines = freeLines - (int)Math.Ceiling((decimal)dispMesgBuf.Length / (decimal)width);
+                            Console.WriteLine(dispMesgBuf);
+                        }
+                    }
+                    else if (MessagesIndex + peekLines == MessagesBuffer.Count) {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.WriteLine("[New Message...]");
+                        freeLines = 0;
+                    }
+                    else {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("[Creating New Message]");
+                        freeLines = 0;
+                        // we need to do THIS or the program will softlock
+                        // (while freeLines > 1) - 1 is for the cursor line since we use Console.WriteLine
+                    }
+                }
+
                 Console.ResetColor();
-                Console.WriteLine(senderBuffer);
-                Console.WriteLine(messageBuffer);
+
                 ConsoleKeyInfo keyInfo = Console.ReadKey(true);
                 if (keyInfo.Key == ConsoleKey.Enter) {
                     if (MessagesIndex < MessagesBuffer.Count) {
                         MessagesBuffer[MessagesIndex].Sender = senderBuffer;
                         MessagesBuffer[MessagesIndex].Text = messageBuffer;
+                        MessagesBuffer[MessagesIndex].Type = MsgType;
                     }
                     else {
                         MessagesBuffer.Add(new Message {
                             Sender = senderBuffer,
                             Text = messageBuffer,
-                            Type = MessageType.Sent });
+                            Type = MsgType });
                         MessagesIndex += 1;
                         messageBuffer = "";
                     }
@@ -60,7 +124,12 @@ namespace MomoHTMLEditor
                     break;
                 }
                 else if (keyInfo.Key == ConsoleKey.Tab) {
-                    activeSenderBuffer = !activeSenderBuffer;
+                    if (MsgType == MessageType.Received) {
+                        activeSenderBuffer = !activeSenderBuffer;
+                    }
+                    else {
+                        activeSenderBuffer = false;
+                    }
                 }
                 else if (keyInfo.Key == ConsoleKey.UpArrow || keyInfo.Key == ConsoleKey.DownArrow) {
                     MessagesIndex += (keyInfo.Key == ConsoleKey.UpArrow ? -1 : 1);
@@ -68,10 +137,23 @@ namespace MomoHTMLEditor
                     if (MessagesIndex < MessagesBuffer.Count) {
                         senderBuffer = MessagesBuffer[MessagesIndex].Sender;
                         messageBuffer = MessagesBuffer[MessagesIndex].Text;
+                        MsgType = MessagesBuffer[MessagesIndex].Type;
+                    }
+                    if (MsgType != MessageType.Received) { activeSenderBuffer = false; }
+                }
+                else if (keyInfo.Key == ConsoleKey.LeftArrow || keyInfo.Key == ConsoleKey.RightArrow) {
+                    if (keyInfo.Modifiers.HasFlag(ConsoleModifiers.Alt)) {
+                        // I know what you're thinking. This bit is insane too.
+                        // But holy FUCK if I don't already wanna fucking lose it.
+                        if (keyInfo.Key == ConsoleKey.LeftArrow) {
+                            MsgType = (MessageType)((MsgType == 0) ? 2 : (int)MsgType - 1);
+                        }
+                        else if (keyInfo.Key == ConsoleKey.RightArrow) {
+                            MsgType = (MessageType)(((int)MsgType == 2) ? 0 : MsgType + 1);
+                        }
+                        if (MsgType != MessageType.Received) { activeSenderBuffer = false; }
                     }
                 }
-                //else if (keyInfo.Key == ConsoleKey.LeftArrow || keyInfo.Key == ConsoleKey.RightArrow) {
-                //}
                 else if (keyInfo.Key == ConsoleKey.Backspace) {
                     int delNumber = 1;
                     if (keyInfo.Modifiers.HasFlag(ConsoleModifiers.Control)) {
@@ -79,11 +161,6 @@ namespace MomoHTMLEditor
                         int lastSpace = activeBuffer.LastIndexOf(" ", Math.Max(0, activeBuffer.Length));
                         delNumber = activeBuffer.Length - (lastSpace < 0 ? 0 : lastSpace + 1);
                     }
-                    //else {
-
-                    //}
-
-                    //activeBuffer.Remove(activeBuffer.Length - 1, 1);
                     if (activeBuffer.Length >= delNumber) {
                         activeBuffer = activeBuffer.Substring(0, activeBuffer.Length - delNumber);
                     }
