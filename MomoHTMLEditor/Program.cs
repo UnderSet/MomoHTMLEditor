0678874f636bf69e2e36ceb627f35105926d2d25
UnderSet <80137504+UnderSet@users.noreply.github.com>
2026-06-24 20:26:19 +0700

    chore: basic state machine setup (menu/editor separation)
    Setup for editor mode later.

--- MomoHTMLEditor/Program.cs
+++ MomoHTMLEditor/Program.cs
@@ -1,46 +1,55 @@
-﻿int selMenuInd = 0;
-string[] options = { "Open Editor", "New File", "Open File", "Convert Current File", "Save File", "Save File As", "Settings", "Exit" };
-int runState = 1;
+﻿using MomoHTMLEditor;
 
 Console.Title = "MomoHTML Tool";
 
-while (runState == 1)
+int selMenuInd = 0;
+string[] options = { "Open Editor", "New File", "Open File", "Convert Current File", "Save File", "Save File As", "Settings", "Exit" };
+
+runStates currentState = runStates.Menu;
+
+while (currentState != runStates.Exit)
 {
-    Console.Clear();
-    for (int i=0; i < options.Length; i++)
+    switch (currentState)
     {
-        if (i == selMenuInd)
-        {
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine($"> {options[i]}");
-            Console.ResetColor();
-        }
-        else
-        {
-            //Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine($"  {options[i]}");
-        }
-    }
+        case runStates.Menu:
+            Console.Clear();
+            for (int i = 0; i < options.Length; i++)
+            {
+                if (i == selMenuInd)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"> {options[i]}");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    //Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine($"  {options[i]}");
+                }
+            }
 
-    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
 
-    if (keyInfo.Key == ConsoleKey.UpArrow)
-    {
-        selMenuInd = (selMenuInd == 0) ? options.Length - 1 : selMenuInd - 1;
-    }
-    else if (keyInfo.Key == ConsoleKey.DownArrow)
-    {
-        selMenuInd = (selMenuInd == options.Length - 1) ? 0 : selMenuInd + 1;
-    }
-    else if (keyInfo.Key == ConsoleKey.Enter)
-    {
-        switch (selMenuInd)
-        {
-            case 7:
-                runState = 0;
-                Console.Write("k bye");
-                break;
-        }
+            if (keyInfo.Key == ConsoleKey.UpArrow)
+            {
+                selMenuInd = (selMenuInd == 0) ? options.Length - 1 : selMenuInd - 1;
+            }
+            else if (keyInfo.Key == ConsoleKey.DownArrow)
+            {
+                selMenuInd = (selMenuInd == options.Length - 1) ? 0 : selMenuInd + 1;
+            }
+            else if (keyInfo.Key == ConsoleKey.Enter)
+            {
+                switch (selMenuInd)
+                {
+                    case 7:
+                        currentState = runStates.Exit;
+                        break;
+                }
+            }
+            break;
+        default: //editor code is executed from here idk
+            break;
     }
 }
 
