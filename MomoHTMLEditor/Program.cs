bc3eeaa6d9256745e2effeef487698a16d4ace9f
UnderSet <80137504+UnderSet@users.noreply.github.com>
2026-06-26 13:54:34 +0700

    chore: hardcode filename color (to blue)
    Fixes certain edgecases, but doesn't really qualify as a fix... Still enough for a version bump.
    
    Oh, also it's blue now instead of white. Helps being more noticeable.

--- MomoHTMLEditor/Program.cs
+++ MomoHTMLEditor/Program.cs
@@ -23,7 +23,7 @@ while (currentState != runStates.Exit)
             Console.Write($"MomoHTML Tools v{version}");
             Console.ForegroundColor = ConsoleColor.DarkGray;
             Console.Write($" | Compiled {buildDate}{Environment.NewLine}");
-            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine($"Current file: {fileName}");
             for (int i = 0; i < options.Length; i++)
             {
